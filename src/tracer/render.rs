3cc71055950934bc2382a8aba4d2a48700f33b52
mcthouacbb <mcthouacbb@gmail.com>
2026-04-21 07:34:10 -0400

    Tiling

--- src/tracer/render.rs
+++ src/tracer/render.rs
@@ -64,42 +64,52 @@ pub fn render_image(
 ) {
     let width = image.width();
     let height = image.height();
+    assert!(width % 4 == 0 && height % 4 == 0);
 
     let progress_bar = ProgressBar::new((width * height) as u64);
 
     let mut rng = Xoshiro256PlusPlus::from_rng(&mut rand::rng());
 
-    for y in 0..height {
-        for x in 0..width {
-            let mut accum_color = Vec3::ZERO;
+    for ty in 0..height / 4 {
+        for tx in 0..width / 4 {
+            for py in 0..4 {
+                for px in 0..4 {
+                    let x = 4 * tx + px;
+                    let y = 4 * ty + py;
 
-            for _ in 0..spp {
-                let jitter_x = rng.random_range(-0.5..=0.5f32);
-                let jitter_y = rng.random_range(-0.5..=0.5f32);
-                let u = (2.0 * (x as f32 + jitter_x) as f32 - width as f32 + 1.0) / width as f32;
-                let v = -(2.0 * (y as f32 + jitter_y) as f32 - height as f32 + 1.0) / height as f32;
+                    let mut accum_color = Vec3::ZERO;
 
-                let camera_ray = camera.get_ray_dir(u, v, &mut rng);
-                let ray = Ray::new(
-                    camera_mat.transform_pos(&camera_ray.origin()),
-                    camera_mat.transform_dir(&camera_ray.dir()),
-                );
+                    for _ in 0..spp {
+                        let jitter_x = rng.random_range(-0.5..=0.5f32);
+                        let jitter_y = rng.random_range(-0.5..=0.5f32);
+                        let u = (2.0 * (x as f32 + jitter_x) as f32 - width as f32 + 1.0)
+                            / width as f32;
+                        let v = -(2.0 * (y as f32 + jitter_y) as f32 - height as f32 + 1.0)
+                            / height as f32;
 
-                let color = ray_color(&ray, &objects, &mut rng, max_depth);
-                accum_color += color;
+                        let camera_ray = camera.get_ray_dir(u, v, &mut rng);
+                        let ray = Ray::new(
+                            camera_mat.transform_pos(&camera_ray.origin()),
+                            camera_mat.transform_dir(&camera_ray.dir()),
+                        );
+
+                        let color = ray_color(&ray, &objects, &mut rng, max_depth);
+                        accum_color += color;
+                    }
+
+                    let pixel_color = accum_color / spp as f32;
+                    image.put_pixel(
+                        x,
+                        y,
+                        Rgb([
+                            (linear_to_srgb(pixel_color.x()) * 255.0 + 0.5) as u8,
+                            (linear_to_srgb(pixel_color.y()) * 255.0 + 0.5) as u8,
+                            (linear_to_srgb(pixel_color.z()) * 255.0 + 0.5) as u8,
+                        ]),
+                    );
+                    progress_bar.inc(1);
+                }
             }
-
-            let pixel_color = accum_color / spp as f32;
-            image.put_pixel(
-                x,
-                y,
-                Rgb([
-                    (linear_to_srgb(pixel_color.x()) * 255.0 + 0.5) as u8,
-                    (linear_to_srgb(pixel_color.y()) * 255.0 + 0.5) as u8,
-                    (linear_to_srgb(pixel_color.z()) * 255.0 + 0.5) as u8,
-                ]),
-            );
-            progress_bar.inc(1);
         }
     }
 
